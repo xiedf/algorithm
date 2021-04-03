35500a17955a1849b90842c71d5028cef7a53d77
xdf <18758007456@163.com>
2021-04-04 00:38:13 +0800

    finish percolation

--- percolation/src/Percolation.java
+++ percolation/src/Percolation.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
    // creates n-by-n grid, with all sites initially blocked
-   private boolean[][] open;
+   private boolean[][] e;
    private WeightedQuickUnionUF uf;
    private int n;
    private int count;
@@ -14,20 +16,20 @@ public class Percolation {
          throw new IllegalArgumentException("n <= 1");
       }
       this.n = n;
-      open = new boolean[n][n];
+      e = new boolean[n][n];
       uf = new WeightedQuickUnionUF(n * (n + 2));
       return;
    }
 
    // opens the site (row, col) if it is not open already
    public void open(int row, int col) {
-      open[row][col] = true;
+      e[row][col] = true;
       count++;
    }
 
    // is the site (row, col) open?
    public boolean isOpen(int row, int col) {
-      return open[row][col];
+      return e[row][col];
    }
 
    // is the site (row, col) full?
@@ -45,8 +47,25 @@ public class Percolation {
       return isFull(0, 0);
    }
 
+   @Override
+   public String toString() {
+      String s = "\n";
+      for (int i = 0; i < n; i++) {
+         s += Arrays.toString(e[i]) + "\n";
+      }
+      return s;
+   }
+
    // test client (optional)
    public static void main(String[] args) {
+      Percolation p = new Percolation(4);
+      System.out.println(p);
+      System.out.println(p.percolates());
+      for (int i = 0; i < 4; i++) {
+         p.open(i, 0);
+      }
+      System.out.println(p);
+      System.out.println(p.percolates());
    }
 
 }
